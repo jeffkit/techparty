79c9b6fae0b1cc34685e4293fc97c9aa2f9783e7
jeffkit <jeff@fallever.com>
2011-11-15 22:28:46 +0800

    补充一些视图逻辑及兼容PEP代码规范

--- techparty/core/models.py
+++ techparty/core/models.py
@@ -9,19 +9,21 @@ from social_auth.backends.facebook import FacebookBackend
 from social_auth.backends.twitter import TwitterBackend
 from logger import log
 
+
 class UserProfile(models.Model):
-    user = models.ForeignKey(User,verbose_name=u'用户')
-    description = models.TextField(u'简介',null=True,blank=True)
+    user = models.ForeignKey(User, verbose_name=u'用户')
+    description = models.TextField(u'简介', null=True, blank=True)
     is_lecturer = models.BooleanField(u'讲师')
-    avatar = models.URLField(u'头像',null=True,blank=True) # 第三方帐号的头像链接
-    custom_avatar = models.ImageField(u'自定义头像',upload_to='avatars',null=True,blank=True) # 自定义头像,上传时将该头像转换几种size。
+    avatar = models.URLField(u'头像', null=True, blank=True)
+    custom_avatar = models.ImageField(u'自定义头像', upload_to='avatars',
+                                      null=True, blank=True)
     tags = TagField(u'标签')
 
     def avatar_normal(self):
         if not self.custom_avatar:
             return self.avatar
         else:
-            pass # 返回相应size的头像
+            pass  # 返回相应size的头像
 
     def avatar_profile(self):
         # 返回profile页面用的头像，应属中等size图片,facebook为200×200，twitter为128×128
@@ -34,13 +36,14 @@ class UserProfile(models.Model):
         verbose_name = u'用户资料'
         verbose_name_plural = u'用户资料'
 
+
 class UserLink(models.Model):
     """
     用户连接
     """
-    user = models.ForeignKey(User,verbose_name=u'用户')
+    user = models.ForeignKey(User, verbose_name=u'用户')
     link = models.URLField(u'链接')
-    title = models.CharField(u'标题',max_length=50)
+    title = models.CharField(u'标题', max_length=50)
 
     def __unicode__(self):
         return self.title
@@ -49,18 +52,20 @@ class UserLink(models.Model):
         verbose_name = u'用户链接'
         verbose_name_plural = u'用户链接'
 
+
 class Event(models.Model):
     """
     一次沙龙的活动。
     """
-    name = models.CharField(u'名称',max_length=50)
-    alias = models.SlugField(u'标识',max_length=50)
+    name = models.CharField(u'名称', max_length=50)
+    alias = models.SlugField(u'标识', max_length=50)
     intro = models.TextField(u'内容')
-    hash_tag = models.CharField(u'微博标签',max_length=20,blank=True,null=True) # 用于微博、Twitter
+    hash_tag = models.CharField(u'微博标签', max_length=20,
+                                blank=True, null=True)  # 用于微博、Twitter
     start_time = models.DateTimeField(u'开始时间')
     end_time = models.DateTimeField(u'结束时间')
-    creator = models.ForeignKey(User,verbose_name=u'发起人')
-    create_time = models.DateTimeField(u'发起时间',auto_now_add=True)
+    creator = models.ForeignKey(User, verbose_name=u'发起人')
+    create_time = models.DateTimeField(u'发起时间', auto_now_add=True)
     tags = TagField(u'tag')
 
     def __unicode__(self):
@@ -73,15 +78,16 @@ class Event(models.Model):
         verbose_name = u'活动'
         verbose_name_plural = u'活动'
 
+
 class Enroll(models.Model):
     """
     某一次活动的报名纪录
     """
-    event = models.ForeignKey(Event,verbose_name=u'活动')
-    user = models.ForeignKey(User,verbose_name=u'成员')
-    time = models.DateTimeField(u'报名时间',auto_now_add=True)
-    comment = models.CharField(u'报名理由',max_length=140) #yeah,140 chars,a tweet's length
-    is_permited = models.BooleanField(u'允许') # 被通过的报名才能收到邀请邮件
+    event = models.ForeignKey(Event, verbose_name=u'活动')
+    user = models.ForeignKey(User, verbose_name=u'成员')
+    time = models.DateTimeField(u'报名时间', auto_now_add=True)
+    comment = models.CharField(u'报名理由', max_length=140)
+    is_permited = models.BooleanField(u'允许')  # 被通过的报名才能收到邀请邮件
 
     def __unicode__(self):
         return self.user.username
@@ -95,12 +101,13 @@ class Topic(models.Model):
     """
     某次活动的一个主题
     """
-    event = models.ForeignKey(Event,verbose_name=u'活动')
-    title = models.CharField(u'标题',max_length=50)
-    sub_title = models.CharField(u'副标题',max_length=50,null=True,blank=True)
+    event = models.ForeignKey(Event, verbose_name=u'活动')
+    title = models.CharField(u'标题', max_length=50)
+    sub_title = models.CharField(u'副标题', max_length=50,
+                                 null=True, blank=True)
     description = models.TextField(u'简介')
-    author = models.ForeignKey(User,verbose_name=u'讲师')
-    add_time = models.DateTimeField(u'加入时间',auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name=u'讲师')
+    add_time = models.DateTimeField(u'加入时间', auto_now_add=True)
     tags = TagField(u'标签')
 
     def __unicode__(self):
@@ -110,14 +117,18 @@ class Topic(models.Model):
         verbose_name = u'主题'
         verbose_name_plural = u'主题'
 
+
 class Photo(ImageModel):
-    event = models.ForeignKey(Event,verbose_name=u'活动')
-    name = models.CharField(u'名称',max_length=50)
-    description = models.CharField(u'描述',max_length=140,blank=True,null=True)
-    original_image = models.ImageField(u'原图',upload_to='photos/%Y/%m/%d')
-    num_views = models.PositiveIntegerField(u'查看次数',editable=False,default=0)
-    add_time = models.DateTimeField(u'上传时间',auto_now_add=True)
-    add_by = models.ForeignKey(User,verbose_name=u'上传者')
+    event = models.ForeignKey(Event, verbose_name=u'活动')
+    name = models.CharField(u'名称', max_length=50)
+    description = models.CharField(u'描述', max_length=140,
+                                   blank=True, null=True)
+    original_image = models.ImageField(u'原图',
+                                       upload_to='photos/%Y/%m/%d')
+    num_views = models.PositiveIntegerField(u'查看次数',
+                                            editable=False, default=0)
+    add_time = models.DateTimeField(u'上传时间', auto_now_add=True)
+    add_by = models.ForeignKey(User, verbose_name=u'上传者')
 
     def __unicode__(self):
         return self.name
@@ -132,29 +143,33 @@ class Photo(ImageModel):
         image_field = 'original_image'
         save_count_as = 'num_views'
 
+
 class Vote(models.Model):
     """
     某位成员为某个主题所投的票
     """
-    topic = models.ForeignKey(Topic,verbose_name=u'主题')
-    member = models.ForeignKey(User,verbose_name=u'成员')
-    comment = models.CharField(u'评论',max_length=140)
-    time = models.DateTimeField(u'投票时间',auto_now_add=True)
+    topic = models.ForeignKey(Topic, verbose_name=u'主题')
+    member = models.ForeignKey(User, verbose_name=u'成员')
+    comment = models.CharField(u'评论', max_length=140)
+    time = models.DateTimeField(u'投票时间', auto_now_add=True)
 
     class Meta:
         verbose_name = u'投票'
         verbose_name_plural = u'投票'
 
+
 class LiveMessage(models.Model):
     """
     某次活动中的一条直播消息,可能会被同时发送至微博或Twitter。
     """
-    event = models.ForeignKey(Event,verbose_name=u'活动')
-    text = models.CharField(u'消息',max_length=140)
-    create_by = models.ForeignKey(User,verbose_name=u'作者',related_name="createdMessage")
-    create_time = models.DateTimeField(u'发送时间',auto_now_add=True)
-    update_by = models.ForeignKey(User,verbose_name=u'最后修改人',related_name="updatedMessage")
-    update_time = models.DateTimeField(u'更新时间',auto_now=True)
+    event = models.ForeignKey(Event, verbose_name=u'活动')
+    text = models.CharField(u'消息', max_length=140)
+    create_by = models.ForeignKey(User, verbose_name=u'作者',
+                                  related_name="createdMessage")
+    create_time = models.DateTimeField(u'发送时间', auto_now_add=True)
+    update_by = models.ForeignKey(User, verbose_name=u'最后修改人',
+                                  related_name="updatedMessage")
+    update_time = models.DateTimeField(u'更新时间', auto_now=True)
 
     class Meta:
         verbose_name = u'直播消息'
@@ -163,11 +178,12 @@ class LiveMessage(models.Model):
 
 ######## signals ###########
 
-def create_user_profile(sender,instance,created,**kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     """
     创建用户资料
     """
     log.debug('create user profile')
+
     def get_profile():
         try:
             return instance.get_profile()
@@ -176,24 +192,28 @@ def create_user_profile(sender,instance,created,**kwargs):
             return None
 
     if not get_profile():
-        profile = UserProfile(user=instance,description=' ',is_lecturer=False)
+        profile = UserProfile(user=instance, description=' ',
+                              is_lecturer=False)
         profile.save()
 
 
-post_save.connect(create_user_profile,sender=User)
+post_save.connect(create_user_profile, sender=User)
 
-def facebook_extra_values(sender,user,response,details,**kwargs):
+
+def facebook_extra_values(sender, user, response, details, **kwargs):
     """
     连接facebook时，设置用户的头像为facebook头像
     """
     profile = user.get_profile()
-    profile.avatar = 'http://graph.facebook.com/%s/picture?type=large'%response.get('id')
+    profile.avatar = 'http://graph.facebook.com/%s/picture?type=large'\
+                     % response.get('id')
     profile.save()
     return True
 
-pre_update.connect(facebook_extra_values,sender=FacebookBackend)
+pre_update.connect(facebook_extra_values, sender=FacebookBackend)
 
-def twitter_extra_values(sender,user,response,details,**kwargs):
+
+def twitter_extra_values(sender, user, response, details, **kwargs):
     """
     连接twitter时，设置用户的头像为twitter头像
     """
@@ -202,5 +222,4 @@ def twitter_extra_values(sender,user,response,details,**kwargs):
     profile.save()
     return True
 
-pre_update.connect(twitter_extra_values,sender=TwitterBackend)
-
+pre_update.connect(twitter_extra_values, sender=TwitterBackend)
--- techparty/core/views.py
+++ techparty/core/views.py
@@ -1,10 +1,12 @@
 #encoding=utf-8
-from django.shortcuts import render_to_response,get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-from core.models import Event,Topic,Enroll,Vote,Photo
+from core.models import Event, Topic, Enroll, Vote, Photo
+
 
 def index(request):
     """
@@ -13,21 +15,27 @@ def index(request):
     return render_to_response('core/index.html',
                               context_instance=RequestContext(request))
 
+
 def events(request):
     """
     活动列表面
     """
     events = Event.objects.all().order_by('-start_time')
-    return render_to_response('core/events.html',{'events':events},context_instance=RequestContext(request))
+    return render_to_response('core/events.html',
+                              {'events': events},
+                              context_instance=RequestContext(request))
 
-def event(request,id):
+
+def event(request, id):
     """
     活动详情面页视图
     """
-    event = get_object_or_404(Event,pk=id)
+    event = get_object_or_404(Event, pk=id)
     topics = Topic.objects.filter(event=event)
-    enrolls = Enroll.objects.select_related('user').filter(event=event).order_by('-time')[:20]
-    photos = Photo.objects.select_related('add_by').filter(event=event).order_by('-add_time')[:5]
+    enrolls = Enroll.objects.select_related('user')\
+              .filter(event=event).order_by('-time')[:20]
+    photos = Photo.objects.select_related('add_by')\
+             .filter(event=event).order_by('-add_time')[:5]
 
     is_enroll = False
     if request.user.is_authenticated():
@@ -36,40 +44,72 @@ def event(request,id):
             if request.user == enroll.user:
                 is_enroll = True
 
-    return render_to_response('core/event.html',{'event':event,'topics':topics,'enrolls':enrolls,'photos':photos,'is_enroll':is_enroll},
-            context_instance=RequestContext(request))
+    return render_to_response('core/event.html', {'event': event,
+                                                 'topics': topics,
+                                                 'enrolls': enrolls,
+                                                 'photos': photos,
+                                                 'is_enroll': is_enroll},
+                              context_instance=RequestContext(request))
+
 
 @login_required
-def enroll(request,eid):
+def enroll(request, eid):
     """
     用户参与指定的沙龙活动
     """
-    enrolls = Enroll.objects.filter(user=request.user,event__pk=eid)
+    enrolls = Enroll.objects.filter(user=request.user,
+                                    event__pk=eid)
 
     if not enrolls:
-        event = get_object_or_404(Event,pk=eid)
-        comment = request.POST.get('comment',' ')
-        enroll = Enroll(user=request.user,event=event,comment=comment)
+        event = get_object_or_404(Event, pk=eid)
+        comment = request.POST.get('comment', ' ')
+        enroll = Enroll(user=request.user, event=event,
+                        comment=comment)
         enroll.save()
 
-    return HttpResponseRedirect('/event/%s/'%eid)
+    return HttpResponseRedirect('/event/%s/' % eid)
 
-def topic(request,id):
+
+def topics(request):
+    """返回所有的话题
+    Arguments:
+    - `request`:
+    """
+    tps = Topic.objects.filter().select_related(depth=1).order_by('-add_time')
+    return render_to_response('core/topics.html', {'topics', tps},
+                              context_instance=RequestContext(request))
+
+
+def topic(request, id):
     """
     主题页面视图
     """
-    pass
+    tp = get_object_or_404(Topic, pk=id)
+    return render_to_response('core/topic.html', {'topic': tp},
+                              context_instance=RequestContext(request))
+
 
 @login_required
-def vote(request,tid):
+def vote(request, tid):
     """
     用户给某个主题投票
     """
-    pass
+    tp = get_object_or_404(Topic, pk=tid)
+    comment = request.POST.get('comment', ' ')
+    try:
+        Vote.objects.get(topic=tp, member=request.user)
+    except Vote.DoesNotExist:
+        vote = Vote(topic=tp, member=request.user, comment=comment)
+        vote.save()
 
-def member(request,id):
+    return HttpResponseRedirect('/topic/%s/' % tid)
+
+
+def member(request, id):
     """
     成员页面视图
     """
-    pass
+    member = get_object_or_404(User, pk=id)
 
+    return render_to_response('core/member.html', {'member': member},
+                              context_instance=RequestContext(request))
